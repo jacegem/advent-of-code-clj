9b1acc1ab17e12e59a53ef4daac7820b9356dac8
Jace <jacegem@gmail.com>
2022-02-28 16:14:33 +0900

    Fix part-2

--- src/advent_of_code_clj/2018/day_04_v2.clj
+++ src/advent_of_code_clj/2018/day_04_v2.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-clj.2018.day-04-v2
   (:require [clojure.spec.alpha :as s]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.spec.test.alpha :as stest]))
 
 
 ;; https://adventofcode.com/2018/day/4
@@ -14,15 +15,30 @@
 ;; [1518-10-16 00:47] falls asleep
 ;; [1518-10-27 00:02] Guard #3181 begins shift
 
-(s/def ::guard-id int?)
-(s/def ::minutes (s/coll-of int?))
-(s/def ::event (s/keys :req [::guard-id ::year ::month ::day ::hour ::minute ::activity]))
+;; (s/def ::guard-id int?)
+;; (s/def ::minutes (s/coll-of int?))
+;; (s/def ::event (s/keys :req [::guard-id ::year ::month ::day ::hour ::minute ::activity]))
+;; (s/def ::coll-event (s/coll-of ::event))
+;; (s/def ::guard-minute (s/keys :req [::guard-id ::minutes ::count]))
+;; (s/def ::coll-guard-minute (s/coll-of ::guard-minute))
+;; (s/explain)
+;; (s/explain-data (if (s/valid?)))
+
+(s/fdef read-file
+  :args (s/cat :year int? :day int?)
+  :ret (s/coll-of string?))
+;; (stest/instrument `read-file)
+;; (stest/check `read-file)
 
 (defn read-file [year day]
   (->
    (format "resources/%d/input_%02d.txt" year day)
    slurp
    string/split-lines))
+
+(s/fdef parse-int
+  :args (s/cat :guard-id (s/nilable string?))
+  :ret (s/nilable int?))
 
 (defn parse-int
   "문자열을 숫자로 변환"
@@ -39,7 +55,6 @@
   :ret ::event)
 
 (defn input->event
-  "input -> event"
   {:test
    (do
      #(assert (= (input->event "[1518-11-11 00:04] Guard #2179 begins shift")
@@ -59,7 +74,14 @@
      :guard-id (parse-int guard-id)
      :activity (keyword activity)}))
 
-(defn generate-gaurd-sleep-event [{data :data guard :guard sleep-event :sleep-event}
+;; (s/def ::guard-data
+;;   (s/keys :req [::data ::guard ::sleep-event]))
+
+;; (s/fdef generate-guard-sleep-event
+;;   :args (s/alt :event ::event
+;;                :guard-data ::guard-data))
+
+(defn generate-guard-sleep-event [{data :data guard :guard sleep-event :sleep-event}
                                   {activity :activity guard-id :guard-id :as event}]
   (case activity
     :shift {:data (if (contains? data guard-id)
@@ -89,7 +111,7 @@
   (->> (read-file 2018 4)
        sort
        (map input->event)
-       (reduce generate-gaurd-sleep-event {})
+       (reduce generate-guard-sleep-event {})
        :data
        (map sleep-range)))
 
@@ -107,8 +129,8 @@
 
 (defn get-most-minute
   {:test
-   #(assert (= (get-most-minute '(11 12 13 14 15 16 13 14 15 15 16))
-               15))}
+   #(assert (= (get-most-minute '(1 2 3 2 3 4 3))
+               3))}
   [minutes]
   (->> (frequencies minutes)
        (sort-by val)
@@ -124,53 +146,53 @@
 
 
 
+(defn get-guard-id-minute
+  {:test
+   (do #(assert (= (get-guard-id-minute {:guard-id 2 :minutes [4 5]})
+                   '([2 4] [2 5])))
+       #(assert (= (get-guard-id-minute {:guard-id 1 :minutes [2 3 4]})
+                   '([1 2] [1 3] [1 4]))))}
+  [{guard-id :guard-id
+    minutes :minutes}]
+  (map (fn [minute] [guard-id minute]) minutes))
+
+(defn part-2 []
+  (let [data (read-input)
+        guard-id-minute (mapcat get-guard-id-minute data)
+        most-same-minute-count (->> guard-id-minute
+                                    frequencies
+                                    (sort-by val)
+                                    reverse
+                                    first
+                                    second)
+        [guard-id minute] (->> guard-id-minute
+                               frequencies
+                               (filter (fn [[_ v]] (= v most-same-minute-count)))
+                               first
+                               first)]
+    (* guard-id minute)))
 
 (comment
   (part-1)
+  (part-2)
+
   (do
     (test #'parse-int)
     (test #'input->event)
     (test #'sleep-range)
     (test #'get-most-minute)
-    (test #'get-most-sleep))
-  (input->event "[1518-11-11 00:04] Guard #2179 begins shift")
-  (input->event "[1518-09-15 00:38] wakes up")
-  (input->event "[1518-08-14 00:45] falls asleep")
-
-
-  (require '[clojure.spec.test.alpha :as stest])
-  (stest/instrument)
-
-  (->> (read-file 2018 4)
-       sort
-       (map input->event)
-       (reduce generate-gaurd-sleep-event {})
-       :data
-       (map sleep-range)
-       (sort-by :count)
-       reverse
-       first
-       :count)
-
-  (def data (read-input))
-  (:minutes (first (take 1 data)))
-
-
-  (sleep-range [631 [[{:year 1518, :month 2, :day 11, :hour 0, :minute 3, :guard-id nil, :activity :asleep} {:year 1518, :month 2, :day 11, :hour 0, :minute 19, :guard-id nil, :activity :up}]]])
-
-  (def datas
-    [{:guard-id 123, :sleep-event [["event" "event"]]}
-     {:guard-id 456, :sleep-event [["event" "event"]]}])
-
-  (filter #(= 123 (:guard-id %)) datas)
-  (first
-   (filter (fn [data] (= 1234 (:guard-id data))) datas))
+    (test #'get-most-sleep)
+    (test #'get-guard-id-minute))
 
   '())
 
 
-
-
+;; (require '[clojure.spec.test.alpha :as stest])
+;; (get-guard-id-minute {:guard-id 1 :minutes [2 3 4]})
+;; (input->event "[1518-11-11 00:04] Guard #2179 begins shift")
+;; (input->event "[1518-09-15 00:38] wakes up")
+;; (input->event "[1518-08-14 00:45] falls asleep")
+;; (stest/instrument)
 ;; guard-sleep-event
 ;; [
 ;;  {:guard-id 123, :sleep-event [[event event] [event event] [event event]]}
@@ -179,3 +201,55 @@
 ;; (s/def ::sleep-event (s/coll-of (s/coll-of ::event)))
 ;; (s/def ::guard-sleep-event
 ;;   (s/keys :req [::guard-id ::sleep-event]))
+  ;; (->> data
+  ;;      (mapcat get-guard-id-minute)
+  ;;      frequencies
+  ;;      (sort-by vals)
+  ;;      reverse
+  ;;      first
+  ;;      second
+  ;;     ;;  first
+  ;;     ;;  first
+  ;;      )
+
+;; (->> (read-input)
+;;      (mapcat get-guard-id-minute)
+;;      frequencies
+;;      (filter (fn [[_ v]] (= v 17)))
+;;      first)
+
+;; (get-most-same-minute (read-input))
+;; (def guard-id-minute (mapcat get-guard-id-minute (read-input)))
+;; (->> guard-id-minute
+;;      frequencies
+;;      (sort-by val)
+;;      reverse
+;;      first
+;;      second)
+;; (def data (read-input))
+;; (get-most-same-minute data)
+
+;; (->> (read-file 2018 4)
+;;      sort
+;;      (map input->event)
+;;      (reduce generate-guard-sleep-event {})
+;;      :data
+;;      (map sleep-range)
+;;      (sort-by :count)
+;;      reverse
+;;      first
+;;      :count)
+
+;; (def data (read-input))
+;; (:minutes (first (take 1 data)))
+
+
+;; (sleep-range [631 [[{:year 1518, :month 2, :day 11, :hour 0, :minute 3, :guard-id nil, :activity :asleep} {:year 1518, :month 2, :day 11, :hour 0, :minute 19, :guard-id nil, :activity :up}]]])
+
+;; (def datas
+;;   [{:guard-id 123, :sleep-event [["event" "event"]]}
+;;    {:guard-id 456, :sleep-event [["event" "event"]]}])
+
+;; (filter #(= 123 (:guard-id %)) datas)
+;; (first
+;;  (filter (fn [data] (= 1234 (:guard-id data))) datas))
