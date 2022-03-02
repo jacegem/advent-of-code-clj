890b5902cbc532aae318c10e7b93acdcc0dca843
Jace <jacegem@gmail.com>
2022-03-02 11:32:30 +0900

    remove injections

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/test.check "1.1.0"]]
+                ;;  [org.clojure/test.check "1.1.0"]
+                 ]
   ;; https://blog.taylorwood.io/2017/10/15/fspec.html
-  :injections [(require 'lib.core) ;; all instrumented fns should be loaded here
-               (require 'clojure.spec.test.alpha)
-               (clojure.spec.test.alpha/instrument)]
+  ;; :injections [(require 'lib.core) ;; all instrumented fns should be loaded here
+  ;;              (require 'clojure.spec.test.alpha)
+  ;;              (clojure.spec.test.alpha/instrument)]
   :main ^:skip-aot advent-of-code-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
